0611ab6ed015e5be70cd2faea5a9325ddac1abc6
ViliRaunola <raunolavili@gmail.com>
2020-03-21 21:03:56 +0200

    21.3 enää kympin muisti

--- app/src/main/java/com/example/viikko10/MainActivity.java
+++ app/src/main/java/com/example/viikko10/MainActivity.java
@@ -44,15 +44,16 @@ public class MainActivity extends AppCompatActivity {
                 String url = syote.getText().toString();
                 if (url.equals("index.html")){
                     web.loadUrl("file:///android_asset/index.html");
-                    osoite.clear(); //Tyhjennetään aina ennen seuraavaa hakua, jotta osoite löytyis aina kohdasta 0
                     osoite.add("file:///android_asset/index.html");
                     edellinen.add(url);
+                    seuraava.clear();
                 }else{
                     url = "http://" + url;
                     osoite.add(url);
                     int vika = osoite.size() - 1;
                     edellinen.add(url);
                     web.loadUrl(osoite.get(vika));
+                    seuraava.clear();
                 }
             }
         });
@@ -60,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
         paivita_nappi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                web.loadUrl(osoite.get(0));
+                web.loadUrl(osoite.get(osoite.size() - 1));
+                Toast.makeText(getApplicationContext(), "Sivusto päivitetty", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -84,15 +86,20 @@ public class MainActivity extends AppCompatActivity {
                 if(edellinen.size() == 0){
                     Toast.makeText(getApplicationContext(),"Ei ole edellistä", Toast.LENGTH_SHORT).show();
                 }else {
-                    int vika = edellinen.size() - 1; //Listaan add();llä on aina lisätty osoite viimeiselle paikalle, joten haetaan nyt listan viimeinen alkio
-                    edellinen.remove(vika);
-                    if(edellinen.size() == 0){
-                        Toast.makeText(getApplicationContext(),"Ei ole edellistä", Toast.LENGTH_SHORT).show();
-                    }else {
-                        vika = edellinen.size() -1;
+                    if(edellinen.size() > 1) {
+                        int vika = edellinen.size() - 1; //Listaan add();llä on aina lisätty osoite viimeiselle paikalle, joten haetaan nyt listan viimeinen alkio
+                        String temp = edellinen.get(vika);
+                        edellinen.remove(vika);
+                        vika = edellinen.size() - 1;
                         String url = edellinen.get(vika);
-                        seuraava.add(osoite.get(osoite.size() - 1));
-                        web.loadUrl(url);
+                        seuraava.add(temp);
+                        if (url.equals("index.html")) {
+                            web.loadUrl("file:///android_asset/index.html");
+                        }else{
+                            web.loadUrl(url);
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Ei ole edellistä", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -105,8 +112,13 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Ei ole seuraavaa", Toast.LENGTH_SHORT).show();
                 }else{
                     String url = seuraava.get(seuraava.size() - 1);
-                    edellinen.add(osoite.get(osoite.size() - 1));
-                    web.loadUrl(url);
+                    edellinen.add(seuraava.get(seuraava.size() - 1));
+                    seuraava.remove(seuraava.size() - 1);
+                    if (url.equals("index.html")) {
+                        web.loadUrl("file:///android_asset/index.html");
+                    }else{
+                        web.loadUrl(url);
+                    }
                 }
 
             }
