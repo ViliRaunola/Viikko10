76c4d34c3aa12569d908f697e4b874b1609e35d2
ViliRaunola <61086026+ViliRaunola@users.noreply.github.com>
2020-03-21 20:09:53 +0200

    21.3

--- app/src/main/java/com/example/viikko10/MainActivity.java
+++ app/src/main/java/com/example/viikko10/MainActivity.java
@@ -8,6 +8,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     WebView web;
     ArrayList<String> osoite = new ArrayList<>(); //Lista johon tallennetaan käyttäjän syöttämä osoite
-
+    ArrayList<String> edellinen = new ArrayList<>(); //Lista johon tallenetaan käyttäjän edellinen osoite
+    ArrayList<String> seuraava = new ArrayList<>(); //Lista johon tulee "seuraava osoite"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         Button paivita_nappi = (Button) findViewById(R.id.paivita);
         Button shoutout_button = (Button) findViewById(R.id.shoutout_button);
         Button initialize_nappi = (Button) findViewById(R.id.initialize_nappi);
+        Button edellinen_nappi = (Button) findViewById(R.id.edellinen_nappi);
+        Button seuraava_nappi = (Button) findViewById(R.id.seuraava_nappi);
         final EditText syote = (EditText) findViewById(R.id.syote);
 
 
@@ -42,11 +46,13 @@ public class MainActivity extends AppCompatActivity {
                     web.loadUrl("file:///android_asset/index.html");
                     osoite.clear(); //Tyhjennetään aina ennen seuraavaa hakua, jotta osoite löytyis aina kohdasta 0
                     osoite.add("file:///android_asset/index.html");
+                    edellinen.add(url);
                 }else{
                     url = "http://" + url;
-                    osoite.clear(); //Tyhjennetään aina ennen seuraavaa hakua, jotta osoite löytyis aina kohdasta 0
                     osoite.add(url);
-                    web.loadUrl(osoite.get(0));
+                    int vika = osoite.size() - 1;
+                    edellinen.add(url);
+                    web.loadUrl(osoite.get(vika));
                 }
             }
         });
@@ -72,6 +78,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        edellinen_nappi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edellinen.size() == 0){
+                    Toast.makeText(getApplicationContext(),"Ei ole edellistä", Toast.LENGTH_SHORT).show();
+                }else {
+                    int vika = edellinen.size() - 1; //Listaan add();llä on aina lisätty osoite viimeiselle paikalle, joten haetaan nyt listan viimeinen alkio
+                    edellinen.remove(vika);
+                    if(edellinen.size() == 0){
+                        Toast.makeText(getApplicationContext(),"Ei ole edellistä", Toast.LENGTH_SHORT).show();
+                    }else {
+                        vika = edellinen.size() -1;
+                        String url = edellinen.get(vika);
+                        seuraava.add(osoite.get(osoite.size() - 1));
+                        web.loadUrl(url);
+                    }
+                }
+            }
+        });
+
+        seuraava_nappi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(seuraava.size() == 0) {
+                    Toast.makeText(getApplicationContext(), "Ei ole seuraavaa", Toast.LENGTH_SHORT).show();
+                }else{
+                    String url = seuraava.get(seuraava.size() - 1);
+                    edellinen.add(osoite.get(osoite.size() - 1));
+                    web.loadUrl(url);
+                }
+
+            }
+        });
 
     }
 
